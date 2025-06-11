e52cdb4e9e524056181577673ad0f0df401e3e4d
Artem Petrov <artempetrovjob@mail.ru>
2025-06-11 17:50:09 +0300

    fix: убрал any

--- src/app/dnd/DroppableCanvas.tsx
+++ src/app/dnd/DroppableCanvas.tsx
@@ -2,6 +2,7 @@ import { useDroppable } from "@dnd-kit/core";
 import styles from "./DroppableCanvas.module.css";
 import { useCalculatorStore } from "@/store/calculatorStore";
 import * as Blocks from "@/components/CalculatorBlocks";
+import type { BlockType } from "@/types/block";
 
 export const DroppableCanvas = () => {
   const { setNodeRef } = useDroppable({ id: "canvas" });
@@ -12,7 +13,7 @@ export const DroppableCanvas = () => {
     return (
       <div
         key={type}
-        onDoubleClick={() => removeBlock(type as any)}
+        onDoubleClick={() => removeBlock(type as BlockType)}
         style={{ marginBottom: 12 }}
       >
         <Block />
